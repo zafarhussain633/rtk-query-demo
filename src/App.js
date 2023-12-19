72201e59089a773d2978f0e091a1784a94238338
details.wes <details.wes@gmail.com>
2023-12-19 14:13:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { useGetPokemonByNameQuery } from './services/pokemon'
 
-function App() {
+export default function App() {
+  // Using a query hook automatically fetches data and returns query values
+  const [query, setQuery] = React.useState("bulbasaur");
+  const { data, error, isLoading } = useGetPokemonByNameQuery(query)
+  // Individual hooks are also accessible under the generated endpoints:
+  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
+
+  
+  const handleChangeQuery = ()=>{
+    setQuery("ditto")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+      <button onClick={handleChangeQuery}>change query</button>
     </div>
-  );
+  )
 }
-
-export default App;
